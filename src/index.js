f8006455964c7b117effcb53718e407330ae105a
Michael Chan <mijoch@gmail.com>
2019-11-10 13:42:17 -0800

    add lesson on useTranstition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./lessons/insertion2";
+import App from "./lessons/insertion3";
 
 const rootElement = document.getElementById("root");
 
