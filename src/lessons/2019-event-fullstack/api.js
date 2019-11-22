36e77453b124ce8d604d36480f87830e3f086f29
Michael Chan <mijoch@gmail.com>
2019-11-21 17:59:10 -0800

    add lesson for fullstack

---
+++ src/lessons/2019-event-fullstack/api.js
@@ -0,0 +1,37 @@
+// import sleep from "sleep-promise";
+
+export function fetchPokemon(id = "") {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res =>
+    res.json()
+  );
+  // .then(sleep(Math.random() * 3000));
+}
+
+export function suspensify(promise) {
+  let status = "pending";
+  let result;
+  let suspender = promise.then(
+    response => {
+      status = "success";
+      result = response;
+    },
+    error => {
+      status = "error";
+      result = error;
+    }
+  );
+
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      }
+      if (status === "error") {
+        throw result;
+      }
+      if (status === "success") {
+        return result;
+      }
+    }
+  };
+}
